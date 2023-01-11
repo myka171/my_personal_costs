e4f3ecd7d1a6c2ce7db935b112c6169021927706
myka171 <romanzakh181@gmail.com>
2023-01-11 12:23:11 +0100

    123

---
+++ my_costs/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="app">
+    <div class="wrapper">
+        <header>Me Personal Costs</header>
+      <main>
+
+      </main>
+    </div>
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ my_costs/src/components/PaymentsDysplay.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "PaymentsDysplay"
+}
+</script>
+
+<style scoped>
+
+</style>
