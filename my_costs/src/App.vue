bec1a340766ab062128e3758c573850669974a68
myka171 <romanzakh181@gmail.com>
2023-01-11 12:45:25 +0100

    123

--- my_costs/src/App.vue
+++ my_costs/src/App.vue
@@ -3,14 +3,31 @@
     <div class="wrapper">
         <header>Me Personal Costs</header>
       <main>
-
+      <payments-display :items="paymentsList"/>
       </main>
     </div>
 </div>
 </template>
 
 <script>
+import PaymentsDisplay from "./components/PaymentsDisplay";
 
+export default {
+  name: 'App',
+  components: {
+    PaymentsDisplay,
+
+  },
+  data(){
+    return {
+      paymentsList: [],
+
+    }
+  },
+  methods: {
+
+  }
+}
 </script>
 
 <style lang="scss">
