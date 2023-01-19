4a743f055dd16251e3480cfb08ae27f0a240c4a4
myka171 <romanzakh181@gmail.com>
2023-01-19 11:59:51 +0100

    123

--- my_costs/src/App.vue
+++ my_costs/src/App.vue
@@ -3,7 +3,9 @@
     <div class="wrapper">
         <header>Me Personal Costs</header>
       <main>
-      <payments-display :items="paymentsList"/>
+        <button @click="addFormRender = !addFormRender">ADD NEW COSTS</button>
+        <add-payment-form v-show="addFormRender" :render="addFormRender" @addNewPayment="onDataPaymentAdd" @render="addFormRender=false"/>
+        <payments-display :items="paymentsList"/>
       </main>
     </div>
 </div>
@@ -11,21 +13,47 @@
 
 <script>
 import PaymentsDisplay from "./components/PaymentsDisplay";
+import AddPaymentForm from "@/components/AddPaymentForm";
 
 export default {
   name: 'App',
   components: {
     PaymentsDisplay,
+    AddPaymentForm,
 
   },
   data(){
     return {
       paymentsList: [],
-
+      addFormRender: false,
     }
   },
   methods: {
-
+    fetchData () {
+      return [
+        {
+          date: '28.03.2020',
+          category: 'Food',
+          value: 169,
+        },
+        {
+          date: '24.03.2020',
+          category: 'Transport',
+          value: 360,
+        },
+        {
+          date: '24.03.2020',
+          category: 'Food',
+          value: 532,
+        },
+      ]
+    },
+    onDataPaymentAdd(data){
+      this.paymentsList.push(data)
+    }
+    },
+  created() {
+    this.paymentsList = this.fetchData()
   }
 }
 </script>
---
+++ my_costs/src/components/AddPaymentForm.vue
@@ -0,0 +1,53 @@
+<template>
+ <div class="payment-content">
+      <input placeholder="Category" v-model="category">
+      <input placeholder="Date" v-model="date">
+      <input placeholder="Value" v-model="value">
+      <button @click="onSaveClick">Save</button>
+ </div>
+</template>
+
+<script>
+export default {
+  name: "AddPaymentForm",
+  props: {
+    addFormRender: {
+      type: Boolean,
+      default: true,
+    }
+  },
+    data(){
+      return {
+          category: '',
+          date: '',
+          value: 0,
+      }
+    },
+  computed:{
+    getCurrentDate(){
+      const today = new Date();
+      const day = today.getDate();
+      const month = today.getMonth() + 1;
+      const year = today.getFullYear();
+      return `${day}.${month}.${year}`
+    }
+  },
+  methods: {
+    onSaveClick(){
+      const data = {
+        value: this.value,
+        category: this.category,
+        date: this.date || this.getCurrentDate,
+
+      }
+      this.$emit('addNewPayment', data)
+      this.$emit('render')
+    }
+  }
+}
+
+</script>
+
+<style >
+
+</style>
